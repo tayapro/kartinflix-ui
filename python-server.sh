c948c18d6bda9309d763a87def79a48e9ededb96
taya <stipaxa@gmail.com>
2022-09-27 21:51:37 +0100

    added url parameters as env

--- python-server.sh
+++ python-server.sh
@@ -1,1 +1,3 @@
+sed -i '' "s|http://localhost:3001|${ID_BASE_URL:-http://localhost:3001}|g" index.js
+sed -i '' "s|http://localhost:3002|${BACKEND_BASE_URL:-http://localhost:3002}|g" index.js
 python3 -u -m http.server 8080
